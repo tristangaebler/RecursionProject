3531cd34a4a7ddfd224c47f8759c1247e137a4a9
tristangaebler <Tristan.Gaebler@gmail.com>
2016-02-11 08:44:39 -0700

    Moved the crap code

--- src/View/RecursionPanel.java
+++ src/View/RecursionPanel.java
@@ -27,17 +27,6 @@ public class RecursionPanel extends JPanel {
 		inputField = new JTextField(20);
 		displayArea = new JTextArea(10, 20);
 		baseLayout = new SpringLayout();
-		baseLayout.putConstraint(SpringLayout.NORTH, factorialButton, 0, SpringLayout.NORTH, fibonacciButton);
-		baseLayout.putConstraint(SpringLayout.WEST, factorialButton, 12, SpringLayout.EAST, fibonacciButton);
-		baseLayout.putConstraint(SpringLayout.WEST, inputField, 98, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, displayArea, 88, SpringLayout.NORTH, this);
-		baseLayout.putConstraint(SpringLayout.WEST, displayArea, 50, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.EAST, displayArea, -50, SpringLayout.EAST, this);
-		baseLayout.putConstraint(SpringLayout.WEST, fibonacciButton, 74, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.SOUTH, fibonacciButton, -22, SpringLayout.SOUTH, this);
-		baseLayout.putConstraint(SpringLayout.SOUTH, displayArea, -24, SpringLayout.NORTH, fibonacciButton);
-		baseLayout.putConstraint(SpringLayout.SOUTH, inputField, -19, SpringLayout.NORTH, displayArea);
-		
 		
 		setupPanel();
 		setupLayout();
@@ -53,10 +42,21 @@ public class RecursionPanel extends JPanel {
 		this.setBackground(Color.BLUE);
 		displayArea.setWrapStyleWord(true);
 		displayArea.setLineWrap(true);
+		displayArea.setEditable(false);
+		
 	}
 	
 	private void setupLayout() {
-		
+		baseLayout.putConstraint(SpringLayout.NORTH, factorialButton, 0, SpringLayout.NORTH, fibonacciButton);
+		baseLayout.putConstraint(SpringLayout.WEST, factorialButton, 12, SpringLayout.EAST, fibonacciButton);
+		baseLayout.putConstraint(SpringLayout.WEST, inputField, 98, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, displayArea, 88, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, displayArea, 50, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, displayArea, -50, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, fibonacciButton, 74, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, fibonacciButton, -22, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, displayArea, -24, SpringLayout.NORTH, fibonacciButton);
+		baseLayout.putConstraint(SpringLayout.SOUTH, inputField, -19, SpringLayout.NORTH, displayArea);
 	}
 	
 	private void setupListeners() {
